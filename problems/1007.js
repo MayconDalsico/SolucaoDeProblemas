8985cddcf36938317b1273fce2ebd72d23990fc3
MayconDalsico <135442648+MayconDalsico@users.noreply.github.com>
2024-12-15 15:43:13 -0400

    1007
    Leia quatro valores inteiros A, B, C e D. A seguir, calcule e mostre a diferença do produto de A e B pelo produto de C e D segundo a fórmula: DIFERENCA = (A * B - C * D).

---
+++ problems/1007.js
@@ -0,0 +1,13 @@
+export function problem(lines){
+
+  const A = lines[0];
+  const B = lines[1];
+  const C = lines[2];
+  const D = lines[3];
+
+  const diferenca = (Number(A)*Number(B))-(Number(C)*(Number(D)));
+
+  console.log(`DIFERENCA = ${diferenca}`)
+
+
+}
